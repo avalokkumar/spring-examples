ef6eea4b8fe5c7110ffba5a22ba877808727d71a
avalokkumar <avalokkumar@gmail.com>
2016-07-13 12:54:06 +0530

    Dependencies added using bower.json file. sample homepage created with angular js setup

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -1,1 +1,21 @@
-var amgApp = angular.module("amgApp", []);
+var amgApp = angular.module('amgApp', []);
+
+amgApp.controller('HomeController', ['$scope', function ($scope){
+	
+	$scope.desc = "Ashok Motor Garage was estb. in Aug, 1990. Initially, it used to do the fixing of bus's and truck's engine." +
+			"But now, it only focuses on trucks. It also works on installing and fixing of Heavy trucks ABS brakes.";
+	$scope.addPost = function(){
+		return "";
+	};
+	
+	$scope.posts = [{
+				title : "Random Post",
+				link : "http://github.com/avalokkumar/spring-examples",
+				upvotes : 10
+			},
+			{
+				title : "Random Post 2",
+				link : "http://github.com/avalokkumar/spring-core",
+				upvotes : 7
+			}];
+}]);
