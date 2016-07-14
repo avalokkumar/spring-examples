775905a7b6099a2c4663c5020e9871532f8d74f7
avalokkumar <avalokkumar@gmail.com>
2016-07-14 16:48:53 +0530

    Dependencies added. Homepage updated.

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -1,13 +1,17 @@
 (function(){
 	'use strict';
-	var amgApp = angular.module('amgApp', ['ngMaterial']);
-	/*.config(function($mdThemingProvider) {
-		$mdThemingProvider.theme('altTheme')
-		.primaryPalette('green');
-	});*/
+	var amgApp = angular.module('amgApp', ['ngMaterial','ngAnimate','angular-carousel'])
+	.config(function($mdThemingProvider) {
+		  $mdThemingProvider.theme('default')
+		    .dark();
+	});
 	
 	
 	amgApp.controller('HomeController', ['$scope', function ($scope){
+		
+		$scope.url = "../images/bcg2.jpg";
+		$scope.garageImages = ["../images/garage/Garage_1.jpg","../images/garage/Garage_2.jpg","../images/garage/Garage_3.jpg",
+		                       "../images/garage/Garage_4.jpg","../images/garage/Garage_5.jpg","../images/garage/Garage_6.jpg"];
 		
 		$scope.desc = "Ashok Motor Garage was estb. in Aug, 1990. Initially, it used to do the fixing of bus's and truck's engine." +
 				"But now, it only focuses on trucks. It also works on installing and fixing of Heavy trucks ABS brakes.";
